2f00a555cde947d5ac33ec88c8474eb07ab151bb
Gaeph <gaephico22@gmail.com>
2025-07-18 13:15:23 -0700

    update

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -1,16 +1,36 @@
-import { useEffect, useState } from "react";
 
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import ErrorPage from "./pages/ErrorPage";
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/directors")
+      .then((res) => res.json())
+      .then((data) => setDirectors(data));
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directors.map((director) => (
+          <article key={director.name}>
+            <h2>{director.name}</h2>
+            <ul>
+              {director.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Directors;
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+
+import ErrorPage from "./pages/ErrorPage";
 
 function Movie() {
   return (
     <>
       <header>
-        {/* What component should go here? */}
+       <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+      {movie ? (
+          <>
+            <h1>{movie.title}</h1>
+            <p>{movie.duration} minutes</p>
+            {movie.genres.map((genre, index) => (
+              <span key={index}>{genre} </span>
+            ))}
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
       </main>
     </>
   );
